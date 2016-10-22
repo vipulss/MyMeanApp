0c783d98aec673dbd9dafaad278395d6d5c598a6
Vipul Shah <Vipul Shah>
2016-10-22 17:53:16 +0530

    Hello world mean app

---
+++ routes/paymentMode.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/pathDB');
+
+db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error:"));
+    db.once("open", function callback(){
+        console.log("CONNECTED");
+    });
+
+var paymentModeSchema = new mongoose.Schema({ name : String, description : String}, { collection : 'PaymentModes'})
+var paymentMode = mongoose.model('paymentmodes',paymentModeSchema);
+
+var router = express.Router();
+
+router.use(function(req,res,next) {
+
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+router.get('/getAll', function(req,res,next) {
+
+    
+    paymentMode.find({},function(err,docs){
+        res.json(docs);
+        res.end();
+    });
+});
+
+module.exports = router;
+
